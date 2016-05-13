6e00d71ada55b038800b8b703becdf7fa7fdfa80
heniokalinowski <xinus01@gmail.com>
2016-05-13 20:15:42 +0200

    Created hqlExample, extracted jpaExample with additional location and added installation instruction.

--- src/main/java/com/devcrumb/App.java
+++ src/main/java/com/devcrumb/App.java
@@ -1,10 +1,5 @@
 package com.devcrumb;
 
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.devcrumb.dao.EventDao;
 import com.devcrumb.dao.PersonDao;
 import com.devcrumb.model.Event;
@@ -13,6 +8,12 @@ import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Standalone application with Spring Data JPA, Hibernate and Maven
@@ -20,67 +21,101 @@ import com.vividsolutions.jts.io.WKTReader;
  * @author DevCrumb.com
  */
 public class App {
+
+	/*
+	1. zainstalowac postgresa i postgisa: sudo apt-get install -y postgresql postgis
+	2. zalogowac sie na usera postgres: sudo -u postgres psql postgres
+	3. zmienic haslo usera postgres na pasujace do pliku persistence.xml: postgres=# \password postgres
+	4. nadal bedac zalogowanym do bazy, zainstalowac dodatek: CREATE EXTENSION postgis;
+	5. odpalic aplikacje
+	 */
+
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
 		
 		EventDao eventDao = context.getBean(EventDao.class);
 		PersonDao personDao = context.getBean(PersonDao.class);
-		
-		Event event1 = createEvent("Event 1", new Date(), "POINT(10 5)");
-		Person person1 = new Person("John", "Doe");
-		person1.setEvent(event1);
-		event1.getPersons().add(person1);
-		Long storedEventId = eventDao.save(event1).getId();
-		
-		System.out.println("Count Event records: " + eventDao.count());
-		
-		List<Event> events = (List<Event>) eventDao.findAll();
-		for (Event event : events) {
-			System.out.println(event);
-		}
-		/**
-		 * TODO Lazy 
-		 */
-		Event storedEvent = eventDao.getById(storedEventId);
-		System.out.println(storedEvent.getPersons().get(0));
-		
-		Person peter = new Person("Peter", "Sagan");
-		Person nasta = new Person("Nasta", "Kuzminova");
+        EntityManager entityManager = context.getBean(EntityManager.class);
 
-		// Add new Person records
-		personDao.save(peter);
-		personDao.save(nasta);
+        jpaExample(eventDao, personDao);
 
-		// Count Person records
-		System.out.println("Count Person records: " + personDao.count());
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println();
+        }
 
-		// Print all records
-		List<Person> persons = (List<Person>) personDao.findAll();
-		for (Person person : persons) {
-			System.out.println(person);
-		}
-
-		// Find Person by surname
-		System.out.println("Find by surname 'Sagan': "	+ personDao.findBySurname("Sagan"));
-
-		// Update Person
-		nasta.setName("Barbora");
-		nasta.setSurname("Spotakova");
-		personDao.save(nasta);
-
-		System.out.println("Find by id 2: " + personDao.findOne(2L));
-
-		// Remove record from Person
-		personDao.delete(2L);
-
-		// And finally count records
-		System.out.println("Count Person records: " + personDao.count());
+        hqlExample(entityManager);
 
 		context.close();
 	}
-	
-	private static Event createEvent(String title, Date theDate, String wktPoint) {
+
+    private static void jpaExample(EventDao eventDao, PersonDao personDao)
+    {
+        Event event1 = createEvent("Event 1", new Date(), "POINT(10 5)");
+        Person person1 = new Person("John", "Doe");
+        person1.setEvent(event1);
+        event1.getPersons().add(person1);
+        Long storedEventId = eventDao.save(event1).getId();
+
+        Event event2 = createEvent("Event 1", new Date(), "POINT(15 10)");
+        Person person2 = new Person("Marian", "Kowalski");
+        person2.setEvent(event2);
+        event2.getPersons().add(person2);
+        Long storedEventId2 = eventDao.save(event2).getId();
+
+        System.out.println("Count Event records: " + eventDao.count());
+
+        List<Event> events = (List<Event>) eventDao.findAll();
+        for (Event event : events) {
+            System.out.println(event);
+        }
+
+        Event storedEvent = eventDao.getById(storedEventId);
+        System.out.println(storedEvent.getPersons().get(0));
+
+        Person peter = new Person("Peter", "Sagan");
+        Person nasta = new Person("Nasta", "Kuzminova");
+
+        // Add new Person records
+        personDao.save(peter);
+        personDao.save(nasta);
+
+        // Count Person records
+        System.out.println("Count Person records: " + personDao.count());
+
+        // Print all records
+        List<Person> persons = (List<Person>) personDao.findAll();
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+
+        // Find Person by surname
+        System.out.println("Find by surname 'Sagan': "	+ personDao.findBySurname("Sagan"));
+
+        // Update Person
+        nasta.setName("Barbora");
+        nasta.setSurname("Spotakova");
+        personDao.save(nasta);
+
+        System.out.println("Find by id 2: " + personDao.findOne(2L));
+
+        // Remove record from Person
+        personDao.delete(2L);
+
+        // And finally count records
+        System.out.println("Count Person records: " + personDao.count());
+    }
+
+    private static void hqlExample(EntityManager entityManager)
+    {
+        Geometry filter = wktToGeometry("POINT(5 5)");
+        Query query = entityManager.createQuery("select e from Event e where ST_Distance(e.location, :filter) > 10", Event.class);
+        query.setParameter("filter", filter);
+        System.out.println(query.getResultList());
+    }
+
+    private static Event createEvent(String title, Date theDate, String wktPoint) {
         Geometry geom = wktToGeometry(wktPoint);
 
         if (!geom.getGeometryType().equals("Point")) {
